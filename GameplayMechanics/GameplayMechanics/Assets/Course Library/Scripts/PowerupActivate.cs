ad577c8a4dcf27f462879c60bc8b92e3efe7a6ad
ILikeLigma4879 <oakleyo@roverkids.org>
2025-05-29 14:15:13 -0400

    Removes merge conflict markers
    Removes extraneous merge conflict markers from the PowerupActivate script.
    
    The markers were likely introduced during a merge operation
    and are no longer needed now that the conflict is resolved.

--- GameplayMechanics/GameplayMechanics/Assets/Course Library/Scripts/PowerupActivate.cs
+++ GameplayMechanics/GameplayMechanics/Assets/Course Library/Scripts/PowerupActivate.cs
@@ -8,11 +8,7 @@ public class PowerupActivate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-
-=======
         controller = GameObject.Find("Player").GetComponent<PlayerController>();
->>>>>>> 6efd1e1 (Implements power-up functionality)
     }
 
     // Update is called once per frame
